cef2a2f7287c4393d98b8194b18fc4f0e442d6ee
Akhmetov Salavat <salavat_akhmetov_2005@mail.ru>
2024-01-15 11:50:28 +0300

    design-for-services-block

--- src/components/screens/Services/Events/Events.jsx
+++ src/components/screens/Services/Events/Events.jsx
@@ -7,16 +7,17 @@ const Events = () => {
     return (
         <section className="Events">
             <div className="container">
-                <h1>Мероприятия</h1>
-                <div className="right-block">
-                    <div><img src={meeting} alt="встречи" /></div>
+                <h2>Мероприятия</h2>
+                <div className="slider-block">
+                    <div className="slider-content">
+                        <div className="slider">
+                            <div><img src={meeting} alt="встречи" /></div>
+                            <div><img src={parties} alt="вечеринки" /></div>
+                            <div><img src={weddings} alt="свадьбы" /></div>
+                        </div>
+                    </div>
                 </div>
-                <div className="right-block">
-                    <div><img src={parties} alt="вечеринки" /></div>
-                </div>
-                <div className="right-block">
-                    <div><img src={weddings} alt="свадьбы" /></div>
-                </div>
+                <p>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.</p>
             </div>
         </section>
     );
--- src/components/screens/Services/Experiences/Experiences.jsx
+++ src/components/screens/Services/Experiences/Experiences.jsx
@@ -1,14 +1,29 @@
-import beyond from "../../../../media/beyond-the-beach-feature.jpg";
-import seaside from "../../../../media/casa_del_mar_pool_alt_view.jpg";
+import seaside from '../../../../media/casa_del_mar_pool_alt_view.jpg';
+import serf from '../../../../media/serf.jpg';
+import bikes from '../../../../media/bicycles.jpg';
+import health from '../../../../media/health.jpg'
 import "./Experiences.scss"
 
 const Experiences = () => {
     return (
         <section className="Experiences">
             <div className="container">
-                <h1>Опыт</h1>
-                <div><img src={beyond} alt="beyond the sea" /></div>
-                <div><img src={seaside} alt="seaside" /></div>
+                <h2>Опыт</h2>
+                <div><img className='main-photo__Experiences' src={seaside} alt="seaside" /></div>
+                <ul className='items'>
+                    <li className='item'>
+                        <div><img src={serf} alt='serf' /></div>
+                        <p>Научитесь «зависать на 10» на индивидуальных или групповых уроках серфинга от опытных профессионалов в Learn to Surf LA. Индивидуальные уроки начинаются от 140 долларов. Уроки серфинга могут быть разработаны для всех уровней подготовки и атлетизма. </p>    
+                    </li>
+                    <li className='item'>
+                        <div><img src={bikes} alt='bikes' /></div>
+                        <p>Прокатитесь вдоль береговой линии почти на три мили к северу и югу от отеля Casa del Mar на арендованном пляжном круизере. Мы предоставляем карты, указания и бутылку воды; Остальная часть вашего приключения в Санта-Монике зависит от вас! Стоимость аренды пляжных круизеров начинается от 10 долларов за один час, 17 долларов за два часа и 25 долларов за день. Подробности уточняйте у нашей стойки консьержа.</p>    
+                    </li>
+                    <li className='item'>
+                        <div><img src={health} alt='health' /></div>
+                        <p>На фоне мягко плещущихся волн по теплому песку Casa del Mar предоставляет возможность продолжить занятия фитнесом с помощью тренировки на пляже или начать новую в одном из самых безмятежных мест, которые только можно себе представить.</p>    
+                    </li>
+                </ul>
             </div>
         </section>
     )
