15333f12a05d3b6acfc44040bd499c2d87b843bd
GBlackWhiteG <ahms65040@gmail.com>
2024-01-16 17:13:54 +0300

    footer-design

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,22 @@
+import { Link } from 'react-router-dom';
+import logo from '../../media/casa-del-mar--logo.svg';
 import './Footer.scss';
 
 const Footer = () => {
   return (
     <section className='Footer'>
       <div className='container'>
-        
+        <div className='top-footer'>
+          <div>
+            <Link to="/" className='logo-wrapper footer-link' >
+              <img className='logo' src={logo} alt='footer-logo' />
+              <h3>Casa del Mar</h3>
+            </Link>
+          </div>
+        </div> 
+        <div className='bottom-footer'> 
+          
+        </div>
       </div>
     </section>
   );
