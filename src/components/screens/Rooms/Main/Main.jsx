68f34c21ff5a78ee1cf333271eb3770413f24fa3
GBlackWhiteG <ahms65040@gmail.com>
2024-01-22 22:23:11 +0300

    rooms wtih grid

--- src/components/screens/Rooms/Main/Main.jsx
+++ src/components/screens/Rooms/Main/Main.jsx
@@ -1,7 +1,8 @@
 import { Link } from 'react-router-dom';
-import roomsInfo from '../../../services/importPicturesInfo'
-import "./Main.scss"
 import { useState } from 'react';
+import roomsInfo from '../../../services/importPicturesInfo'
+import Button from "../../../ui/Button/Button"
+import "./Main.scss"
 
 const Main = () => {
     const [hitRoom, selectHitRoom] = useState(1);
@@ -21,15 +22,18 @@ const Main = () => {
                                             <span className='property'>37</span>
                                             <span>кв. м</span>
                                         </li>
-                                        <li className='item__properties'>
+                                        <div className='item-line' />
+                                        <li className='item__properties center-item'>
                                             <span className='property'>1</span>
                                             <span>спальня</span>
                                         </li>
+                                        <div className='item-line' />
                                         <li className='item__properties'>
                                             <span className='property'>1</span>
                                             <span>кровать</span>
                                         </li>
                                     </ul>
+                                    <Button className="item-button" text="Подробнее" />
                                 </div>
                             </Link> 
                         </li>
