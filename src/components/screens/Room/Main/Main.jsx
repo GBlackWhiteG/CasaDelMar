90ca111d504273a647f10fec97beb8e89b6c6e5b
GBlackWhiteG <ahms65040@gmail.com>
2024-01-24 11:01:38 +0300

    +- all design

--- src/components/screens/Room/Main/Main.jsx
+++ src/components/screens/Room/Main/Main.jsx
@@ -6,8 +6,19 @@ const Main = (id) => {
         <section className='Main__room'>
             <div className='image-wrapper'><img src={roomsInfo[id.index].src} /></div>
             <div className='container'>
-                <h1>{roomsInfo[id.index].name}</h1>
+                <h2>{roomsInfo[id.index].name}</h2>
                 <p>{roomsInfo[id.index].description}</p>
+                <p className='features'>1 кровать / 1 кровать / вид на океан / 1 полноразмерная кровать диван-кровать</p>
+                <div className='comfort'>
+                    <h3>Удобства</h3>
+                    <ul className='comfort-items'>
+                        <li>37 кв. м</li>
+                        <li>Smart TV и акустическая система bluetooth</li>
+                        <li>Ванная комната из итальянского мрамора</li>
+                        <li>Постельное белье итальянского дизайна</li>
+                        <li>Кофемашина Неспрессо</li>
+                    </ul>
+                </div>
             </div> 
         </section>
     );
